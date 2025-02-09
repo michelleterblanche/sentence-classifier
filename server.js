78f074f103faebbf2b0de27baf1d0d7a82958373
michelleterblanche <michelle.terblanche@gmail.com>
2025-02-09 17:01:01 +0200

    Update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const mysql = require('mysql2');
 const fs = require('fs');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Create MySQL connection to Azure
 const db = mysql.createPool({
